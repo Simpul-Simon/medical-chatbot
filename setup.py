a43bd5ce44b25ed0411986459ee8646becfce536
Simpul Simon <simpulsimonrout@gmail.com>
2025-04-02 02:23:32 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='generative_ai_project',  # Use a more concise name
+    version='0.0.0',
+    author='Simpul Simon',
+    author_email='simpulsimonrout@gmail.com',
+    packages=find_packages(),  # Automatically finds your packages
+    install_requires=[  # If you have dependencies, list them here
+        # Example:
+        # 'requests', 
+        # 'flask',
+    ],
+)
